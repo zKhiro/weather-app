69d3e55eb9aed5b0500a37bd5c49fa311e822d12
Leandro <leandro@Mac-mini-de-Tokenlab.local>
2020-11-10 15:31:58 -0300

    Integrate AccuWeather API

--- js/forecast.js
+++ js/forecast.js
@@ -3,4 +3,23 @@ const key           = ''; // Put your key from AccuWeather here.
 
 const endpoints = {
   cityLocation: 'locations/v1/cities/search',
+  cityWeather:  'currentconditions/v1/',
+};
+
+const getCity = async city => {
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response  = await fetch(baseEndpoint + endpoints.cityLocation + query);
+  const data      = await response.json();
+
+  return data[0];
+};
+
+const getWeather = async cityKey => {
+  const query = `?apikey=${key}`;
+
+  const response  = await fetch(baseEndpoint + endpoints.cityWeather + cityKey + query);
+  const data      = await response.json();
+
+  return data[0];
 }
