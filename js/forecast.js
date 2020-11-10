b5643bb09a149cff9d0434d8bfd3f3690f570fb8
Leandro <leandro@Mac-mini-de-Tokenlab.local>
2020-11-10 14:03:47 -0300

    Adding template

---
+++ js/forecast.js
@@ -0,0 +1,6 @@
+const baseEndpoint  = 'http://dataservice.accuweather.com/';
+const key           = ''; // Put your key from AccuWeather here.
+
+const endpoints = {
+  cityLocation: 'locations/v1/cities/search',
+}
